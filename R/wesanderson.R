45af712a3dd9205331a9e0903cdccf5cf5a6177b
CodeByHarri <harri7459@gmail.com>
2020-09-22 17:15:49 +1000

    fix

--- R/wesanderson.R
+++ R/wesanderson.R
@@ -1,6 +1,6 @@
-#' @title ColourSauce
-#' @name ColourSauce
+#' @title wesanderson
+#' @name wesanderson
 #' @docType package
-#' @details list of palettes from the sauce
-#' @description list of palettes from the sauce
+#' @details list of palettes from Wes Anderson movies
+#' @description list of palettes from Wes Anderson movies
 NULL
